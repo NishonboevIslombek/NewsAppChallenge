a06d6ae15875917d07f4efe567871b0408428b1d
islombeknishonboev <nishonboevislombek@gmail.com>
2023-11-18 22:50:41 +0500

    feat: plugins(kapt, hilt) & dependencies(hilt,viewmodel-compose) added to build_gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -30,12 +32,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+
+    kotlin {
+        jvmToolchain(17)
     }
+
     buildFeatures {
         compose = true
     }
@@ -77,5 +81,14 @@ dependencies {
     //Navigation - Compose
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
+    //ViewModel compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+}
+
+kapt {
+    correctErrorTypes = true
 }

f0d0d2f06af086ad8545a4e16d5dd5c9a415acc7
islombeknishonboev <nishonboevislombek@gmail.com>
2023-12-04 18:38:26 +0500

    refactor: two new funcs(bindRecentNewsUseCase & bindPopularNewsUseCase) added to UseCaseModule

--- app/src/main/java/com/example/newsapplicationchallenge/di/module/UseCaseModule.kt
+++ app/src/main/java/com/example/newsapplicationchallenge/di/module/UseCaseModule.kt
@@ -1,14 +1,25 @@
 package com.example.newsapplicationchallenge.di.module
 
+import com.example.newsapplicationchallenge.data.use_case.RecentNewsUseCaseImpl
+import com.example.newsapplicationchallenge.data.use_case.PopularNewsUseCaseImpl
 import com.example.newsapplicationchallenge.data.use_case.TopNewsUseCaseImpl
+import com.example.newsapplicationchallenge.domain.use_case.RecentNewsUseCase
+import com.example.newsapplicationchallenge.domain.use_case.PopularNewsUseCase
 import com.example.newsapplicationchallenge.domain.use_case.TopNewsUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+
 @InstallIn(SingletonComponent::class)
 @Module
 interface UseCaseModule {
     @Binds
-     fun bindTopNewsUseCase(repository: TopNewsUseCaseImpl): TopNewsUseCase
+    fun bindTopNewsUseCase(impl: TopNewsUseCaseImpl): TopNewsUseCase
+
+    @Binds
+    fun bindRecentNewsUseCase(impl: RecentNewsUseCaseImpl): RecentNewsUseCase
+
+    @Binds
+    fun bindPopularNewsUseCase(impl: PopularNewsUseCaseImpl): PopularNewsUseCase
 }

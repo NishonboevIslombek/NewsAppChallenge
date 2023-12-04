d7c6a7243f84ed0ae3a7851c154abc9181932f1c
islombeknishonboev <nishonboevislombek@gmail.com>
2023-12-04 18:28:22 +0500

    feat: getPopularNews() fun added to HomeViewModel

--- app/src/main/java/com/example/newsapplicationchallenge/presentation/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/newsapplicationchallenge/presentation/ui/home/HomeViewModel.kt
@@ -4,17 +4,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.newsapplicationchallenge.data.network.common.Resource
 import com.example.newsapplicationchallenge.domain.model.NewsItem
+import com.example.newsapplicationchallenge.domain.use_case.RecentNewsUseCase
+import com.example.newsapplicationchallenge.domain.use_case.PopularNewsUseCase
 import com.example.newsapplicationchallenge.domain.use_case.TopNewsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val topNewsUseCase: TopNewsUseCase
+    private val topNewsUseCase: TopNewsUseCase,
+    private val allNewsUseCase: RecentNewsUseCase,
+    private val popularNews: PopularNewsUseCase
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(HomeUiState())
@@ -22,10 +27,12 @@ class HomeViewModel @Inject constructor(
         get() = _uiState
 
     init {
-        getNews()
+        getTopHeadlines().onAwait
+        getRecentNews().onAwait
+        getPopularNews().onAwait
     }
 
-    private fun getNews() {
+    private fun getTopHeadlines(): Deferred<Unit> {
 
         _uiState.update { currentUiState ->
             currentUiState.copy(
@@ -33,14 +40,14 @@ class HomeViewModel @Inject constructor(
             )
         }
 
-        viewModelScope.launch {
+        return viewModelScope.async {
             when (val response = topNewsUseCase()) {
                 is Resource.Success -> {
                     _uiState.update {
                         it.copy(
                             isLoading = false,
                             isSuccess = true,
-                            topNews = response.data.articles
+                            topHeadlines = response.data.articles
                         )
                     }
                 }
@@ -50,20 +57,83 @@ class HomeViewModel @Inject constructor(
                         it.copy(
                             isLoading = false,
                             isSuccess = true,
-                            topNews = emptyList()
+                            topHeadlines = emptyList()
                         )
                     }
                 }
             }
         }
-
-
     }
 
+    private fun getRecentNews(): Deferred<Unit> {
+        _uiState.update { currentUiState ->
+            currentUiState.copy(
+                isLoading = true
+            )
+        }
+        return viewModelScope.async {
+            when (val response = allNewsUseCase()) {
+                is Resource.Success -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            isSuccess = true,
+                            recentNews = response.data.articles
+                        )
+                    }
+                }
+
+                else -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            isSuccess = true,
+                            recentNews = emptyList()
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getPopularNews(): Deferred<Unit> {
+
+        _uiState.update { currentUiState ->
+            currentUiState.copy(
+                isLoading = true
+            )
+        }
+
+        return viewModelScope.async {
+            when (val response = popularNews()) {
+                is Resource.Success -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            isSuccess = true,
+                            popularNews = response.data.articles
+                        )
+                    }
+                }
+
+                else -> {
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            isSuccess = true,
+                            popularNews = emptyList()
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 data class HomeUiState(
     val isLoading: Boolean = false,
     val isSuccess: Boolean = false,
-    val topNews: List<NewsItem> = emptyList()
+    val topHeadlines: List<NewsItem> = emptyList(),
+    val recentNews: List<NewsItem> = emptyList(),
+    val popularNews: List<NewsItem> = emptyList()
 )
